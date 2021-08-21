668c70ad7be19e400d460ca9219564386b65715e
GusBedasi <ggustavobedasi@gmail.com>
2021-08-21 17:50:29 -0300

    First commit

---
+++ LSPConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LSPLibrary;
+
+namespace LSPConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accountVP = new Manager();
+
+            accountVP.FirstName = "Emma";
+            accountVP.LastName = "Stone";
+            accountVP.CalculatePerHourRate(4);
+
+            var emp = new Employee();
+
+            emp.FirstName = "Gustavo";
+            emp.LastName = "Bedasi";
+            emp.AssignManager(accountVP);
+            emp.CalculatePerHourRate(2);
+
+            Console.WriteLine($"{ emp.FirstName }'s salaryis ${ emp.Salary }/hour");
+
+            Console.WriteLine("Press enter to exit...")
+            Console.ReadLine();
+        }
+    }
+}
